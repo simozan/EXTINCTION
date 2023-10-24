d30ca7cd7147464f18d5f7d5ba577ad4c8a13d27
“Simone <Simone.zanni.87@gmail.com>
2023-10-24 10:55:40 +0200

    enemiesfall

--- dino.js
+++ dino.js
@@ -8,6 +8,24 @@ class Dino {
 
 let dino = new Dino
 
+class Enemy {
+    constructor(areaWidth, areaHeight, element) {
+        this.enemyElement = element;
+        this.areaWidth = areaWidth;
+        this.areaHeight = areaHeight;
+        this.width = 30;
+        this.height = 30;
+        this.isfalling = true;
+        this.verticalPosition = 0;
+        this.horizontalPosition = Math.floor(Math.random() * (areaWidth - this.width));
+        this.enemyElement.style.width = `${this.width} px`;
+        this.enemyElement.style.height = `${this.height} px`;
+        this.enemyElement.style.left = `${this.horizontalPosition}px`;
+    }
+}
+
+
+
 function moveDino(event) {
     if (event.key === "ArrowLeft") {
         if (dino.dinoPositionX === 0) { dino.dinoPositionX = 0 }
@@ -22,32 +40,35 @@ function moveDino(event) {
 document.addEventListener("keydown", moveDino)
 
 
-
-
-
-//let enemyPositionY= 0;
 let FrameCount = 0;
 function GameLoop() {
     requestAnimationFrame(GameLoop);
     FrameCount++
-    //moveEnemy()
+    moveEnemy()
     if (FrameCount % 300 === 0) {
         addEnemy()
     }
 }
 
-const gameArea= document.querySelector("#game-area")
+const gameArea = document.querySelector("#game-area")
+
+let enemyArray = [];
 
 function addEnemy() {
     console.log("ciao")
-    const enemy = document.createElement("div")
-    gameArea.appendChild(enemy)
+    const enemy = document.createElement("div");
     enemy.classList.add("enemy");
+    //const newEnemy= new Enemy (this.boardWidth, this.boardHeight, enemy)
+    const newEnemy = new Enemy(940, 560, enemy)
+    enemyArray.push(newEnemy)
+    gameArea.appendChild(enemy)
 }
 
-//function moveEnemy() {
-  //  enemyPositionY++
-    //enemy.style.top=`${enemyPositionY}px`
-//}
+function moveEnemy() {
+    enemyArray.forEach((oneEnemy) => {
+        oneEnemy.verticalPosition++
+        oneEnemy.enemyElement.style.top = `${oneEnemy.verticalPosition}px`;
+    })
+}
 
 GameLoop()
